02f28a333d5a3d9ad12bcbee3d80126a77b9e824
EthanDGee <workethangee@gmail.com>
2025-12-08 17:34:05 -0700

    Added an id to `Clause`
    - Must be a non-negative number, it has a default value of -1, and can
    be changed once
    - Also refactored constructors for better readability

--- src/main/java/Clause.java
+++ src/main/java/Clause.java
@@ -6,14 +6,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Clause {
+
+    private int id; // Non negative number (defaults to -1 if there is no id)
     private Set<Literal> literals;
 
     public Clause() {
-        this.literals = new HashSet<>();
+        this(-1, new HashSet<Literal>());
     }
 
     public Clause(Set<Literal> literals) {
-        this.literals = new HashSet<>(literals);
+        this(-1, literals);
+    }
+
+    public Clause(int id, Set<Literal> literals) {
+        this.id = id;
+        this.literals = literals;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        if (id <= 0)
+            throw new IllegalArgumentException("ID must be a postive number");
+
+        // this can only be set once
+        if (id != -1)
+            throw new IllegalStateException("ID has already been set (" + this.id + ").");
+        this.id = id;
     }
 
     public void addLiteral(Literal literal) {
