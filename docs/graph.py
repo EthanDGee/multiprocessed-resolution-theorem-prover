e17151e2ecb4fa5069a5be83c778d8a99bc32d01
EthanDGee <workethangee@gmail.com>
2025-12-11 21:50:56 -0700

    Switched to averaged results.csv and added performance improvement graph

--- docs/graph.py
+++ docs/graph.py
@@ -8,12 +8,21 @@ df = pd.read_csv("docs/results.csv")
 plt.figure(figsize=(10, 6))
 plt.plot(df["n"], df["Single Threaded(ms)"], marker="o", label="Single Threaded (ms)")
 plt.plot(df["n"], df["Multi Threaded (ms)"], marker="o", label="Multi Threaded (ms)")
-
-plt.xlabel("n")
+plt.xlabel("Number of Clauses")
 plt.ylabel("Time (ms)")
 plt.yscale("log", base=2)
-plt.title("Single Threaded vs Multi Threaded Performance")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.savefig("docs/performance_graph.png")
+
+
+# plot the performacne imrovement over single threaded
+df["improvement"] = df["Single Threaded(ms)"] / df["Multi Threaded (ms)"]
+plt.figure(figsize=(10, 6))
+plt.plot(df["n"], df["improvement"], marker="o")
+plt.xlabel("Number of Clauses")
+plt.ylabel("Improvement")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("docs/performance_improvement.png")
